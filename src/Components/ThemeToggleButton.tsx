304dbb2e922751d6b8c386759c0e594d2b27b600
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-03-14 20:28:03 +0500

    Removed Icons

--- src/Components/ThemeToggleButton.tsx
+++ src/Components/ThemeToggleButton.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { Button } from "@heroui/react";
 import { useTheme } from "next-themes";
-import ZapIcon from "./Icons/ZapIcon";
-import ZapBulkIcon from "./Icons/ZapBulkIcon";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
@@ -14,8 +12,8 @@ export default function ThemeSwitcher() {
         className="rounded-full outline-none border-none w-20 h-20 flex items-center justify-center text-center"
       >
         { theme === "dark" ? 
-            <ZapIcon className="w-6 h-6 text-gray-900 dark:text-white" color="#f3f3f3" /> 
-            : <ZapBulkIcon className="w-6 h-6 text-gray-400 dark:text-white" color="#f3f3f3" /> 
+            <h3 className="text-white">Dark</h3> 
+            : <h3 className="text-white">Light</h3> 
         }
       </Button>
     </div>
