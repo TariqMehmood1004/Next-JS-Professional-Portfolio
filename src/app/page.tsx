36725e8e9fb8fe6f0e5cf8f087a105207e05913e
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-03-19 23:07:49 +0500

    Fixed Responsive sizing...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,13 +32,13 @@ export default function Home() {
             <MenuToggleButton />
             <Link
               href="/"
-              className="text-2xl transition-all duration-300 md:text-4xl font-medium text-neutral-600 hover:text-[#474747] uppercase"
+              className="text-lg transition-all duration-300 md:text-4xl font-medium text-neutral-600 hover:text-[#474747] uppercase"
             >
               johnbrrighte@engineer.com
             </Link>
           </div>
 
-          <div className="w-full cursor-pointer text-2xl md:text-3xl p-3 px-6 flex gap-10 items-center font-normal text-neutral-500 hover:text-[#474747] uppercase transition-all duration-300">
+          <div className="w-full cursor-pointer text-lg md:text-3xl p-3 px-6 flex gap-10 items-center font-normal text-neutral-500 hover:text-[#474747] uppercase transition-all duration-300">
             <DownRightArrowIcon size={32} color="#cdcdcd" />
             <span className="w-60">Multi-talented Programmer</span>
           </div>
@@ -55,28 +55,28 @@ export default function Home() {
         <Link
           target="_blank"
           href="https://www.google.com/search?..."
-          className="text-2xl md:text-4xl border-2 border-[#171717] rounded-full p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
+          className="text-lg md:text-4xl border-2 border-[#171717] rounded-full p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
         >
           Creative Developer
         </Link>
         <Link
           target="_blank"
           href="http://github.com/TariqMehmood1004/"
-          className="absolute left-[35%] top-20 text-2xl md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
+          className="absolute left-[35%] top-20 text-lg md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
         >
           GitHub
         </Link>
         <Link
           target="_blank"
           href="https://www.linkedin.com/in/tariq-mehmood-3ab013254/"
-          className="absolute left-[45%] top-40 md:top-28 text-2xl md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
+          className="absolute left-[45%] top-40 md:top-28 text-lg md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium hover:bg-[#171717] hover:text-white transition-all duration-300 uppercase"
         >
           LinkedIn
         </Link>
         <Link
           target="_blank"
           href="https://www.linkedin.com/in/tariq-mehmood-3ab013254/"
-          className="absolute left-8 md:left-[50%] top-60 md:top-50 text-2xl md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium bg-[#272727] text-white hover:bg-[#373737] hover:text-white transition-all duration-300 uppercase"
+          className="absolute left-8 md:left-[50%] top-60 md:top-50 text-lg md:text-4xl border-2 border-[#171717] rounded-lg p-4 px-6 font-medium bg-[#272727] text-white hover:bg-[#373737] hover:text-white transition-all duration-300 uppercase"
         >
           Download Resume
         </Link>
@@ -88,7 +88,7 @@ export default function Home() {
         className="mt-60 md:my-40 md:mx-auto w-full md:p-10 pb-3 flex flex-col md:flex-row justify-center md:justify-end items-center gap-10"
       >
         <div className="w-full flex flex-col justify-center items-center gap-5">
-          <h1 className="text-6xl md:text-[12rem] flex justify-center items-end gap-5">
+          <h1 className="text-4xl md:text-[12rem] flex justify-center items-end gap-5">
             <span className="font-bold">Portfolio</span>
             <motion.span
               className="w-3 h-3 md:w-5 md:h-5 bg-blue-500 rounded-full"
@@ -96,7 +96,7 @@ export default function Home() {
               transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
             />
           </h1>
-          <p className="text-lg text-center md:text-left font-normal px-0 md:px-10">
+          <p className="text-sm md:text-2xl text-center md:text-left font-normal px-0 md:px-10">
             As a <strong>Machine Learning enthusiast</strong>, I thrive on pushing technological boundaries.
             Beyond development, I'm dedicated to <strong>knowledge sharing</strong>, empowering others as a <strong>mentor and educator</strong> in software engineering.
             My commitment to transforming ideas into reality reflects my <strong>drive for excellence and innovation</strong> in the ever-evolving tech landscape.
@@ -115,7 +115,7 @@ export default function Home() {
               alt="Profile Picture"
               width={500}
               height={500}
-              className="xm:w-50 xm:h-50 animate-pulse hover:scale-[1.1] transition-all duration-300 rounded-full object-cover"
+              className="w-50 h-50 md:w-96 md:h-96 animate-pulse hover:scale-[1.1] transition-all duration-300 rounded-full object-cover"
             />
           </motion.div>
 
