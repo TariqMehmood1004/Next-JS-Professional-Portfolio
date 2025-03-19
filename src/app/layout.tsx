2d2f51cb2aa3f81373f9ab77d123c2cab8922732
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-03-20 00:35:55 +0500

    Fixed Responsive sizing...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       // Jab loader complete ho jaye, tab wapas theme color set karein
       const metaThemeColor = document.querySelector('meta[name="theme-color"]');
       if (metaThemeColor) {
-        metaThemeColor.setAttribute("content", theme === "dark" ? "#272727" : "#ffffff");
+        metaThemeColor.setAttribute("content", theme === "dark" ? "#272727" : "#272727");
       }
     }, 3000);
   }, [theme]);
@@ -44,7 +44,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/Portfolio.png" sizes="any" />
-        <meta name="theme-color" content={isLoading ? "#FFAF00" : theme === "dark" ? "#272727" : "#ffffff"} />
+        <meta name="theme-color" content={isLoading ? "#FFAF00" : theme === "dark" ? "#272727" : "#272727"} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased px-5 md:px-0 overflow-x-hidden`}
