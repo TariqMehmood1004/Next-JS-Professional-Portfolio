62187bcb5e4163e1f60a344a6b990224b578e42a
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-03-26 22:47:28 +0500

    Resolved route error

--- src/app/api/v1/education/route.ts
+++ src/app/api/v1/education/route.ts
@@ -1,18 +1,19 @@
-
 import APIResponseHandler from "@/Helpers/ApiResponseHandler"
-import { NextResponse, NextRequest } from "next/server"
+import { NextRequest } from "next/server"
 
-export default async function GET(request: NextRequest, response: NextResponse) {
-  const data = [{
-    "id": 1,
-    "user": "user_id here",
-    "title": "Master's in Computer Science",
-    "logo": "Logo here",
-    "organization": "Education name here",
-    "start_date": "2020-01-01",
-    "end_date": "2023-01-01",
-    "description": "Description here"
-  }];
+export async function GET(request: NextRequest) {
+  const data = [
+    {
+      id: 1,
+      user: "user_id here",
+      title: "Master's in Computer Science",
+      logo: "Logo here",
+      organization: "Education name here",
+      start_date: "2020-01-01",
+      end_date: "2023-01-01",
+      description: "Description here"
+    }
+  ];
 
-  return APIResponseHandler.HTTP_200_OK(data, "Education fetched successfully.")
+  return APIResponseHandler.HTTP_200_OK(data, "Education fetched successfully.");
 }
