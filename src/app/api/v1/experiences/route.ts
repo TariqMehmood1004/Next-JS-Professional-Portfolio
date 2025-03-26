0625b920b843a6e9799521c42db750a3df026ea1
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2025-03-26 23:03:07 +0500

    Update route.ts

--- src/app/api/v1/experiences/route.ts
+++ src/app/api/v1/experiences/route.ts
@@ -1,18 +1,19 @@
+import APIResponseHandler from "@/Helpers/ApiResponseHandler";
+import { NextRequest } from "next/server";
 
-import APIResponseHandler from "@/Helpers/ApiResponseHandler"
-import { NextResponse, NextRequest } from "next/server"
+export async function GET(request: NextRequest) {
+  const data = [
+    {
+      id: 1,
+      user: "user_id here",
+      title: "Senior Software Engineer",
+      company: "Company name here",
+      logo: "Logo here",
+      start_date: "2020-01-01",
+      end_date: "2023-01-01",
+      description: "Description here",
+    }
+  ];
 
-export async function GET(request: NextRequest, response: NextResponse): Promise<NextResponse> {
-  const data = [{
-    "id": 1,
-    "user": "user_id here",
-    "title": "Senior software engineer",
-    "company": "Company name here",
-    "logo": "Logo here",
-    "start_date": "2020-01-01",
-    "end_date": "2023-01-01",
-    "description": "Description here",
-  }];
-
-  return APIResponseHandler.HTTP_200_OK(data, "Experiences fetched successfully.")
+  return APIResponseHandler.HTTP_200_OK(data, "Experiences fetched successfully.");
 }
